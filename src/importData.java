42c0750ae2068e0f2d1a8b19439f6274b33aed33
tangxiaolan01 <19931119txl>
2019-06-13 11:33:53 +0800

    gungungun

--- src/importData.java
+++ src/importData.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class importData {
+    public  static  HashMap<Integer,Integer>  maxMap = new HashMap<>();
     public static void main(String args[]) throws InterruptedException {
 /*        long starTime = System.currentTimeMillis();
         writerFast();
@@ -16,40 +17,49 @@ public class importData {
           readerFast();
         long endTime = System.currentTimeMillis();
         System.out.println("读文件需要 " + (endTime - starTime) + "ms");
+        Map.Entry<Integer, Integer> item = maxMap.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).get();
+        System.out.println("maxvalue is: "+ item.getKey() + "max times is :" + item.getValue());
+
+       /* for(int key:maxMap.keySet())
+        {
+            System.out.println("Key: "+key+" Value: "+maxMap.get(key));
+        }*/
+
     }
 
     public static void readerFast() throws InterruptedException {
   /*      for(int i = 0 ;i < 10 ;i++){
             reader(i);
         }*/
-      /*  ExecutorService executorService = Executors.newFixedThreadPool(2);
+/*
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
         for (int i = 0; i < 10; i++) {
             final int index = i;
             executorService.execute(new Runnable() {
                 @Override
                 public void run() {
-                    reader(index);
+                    reader(index,maxMap);
                 }
             });
-               executorService.shutdown();
+
+        }
+        executorService.shutdown();
         executorService.awaitTermination(10, TimeUnit.HOURS);
-        }*/
+*/
 
 
         List<Integer> arr = new ArrayList<>(10);
-        arr.parallelStream().forEach( index->reader(index));
-
+        for ( int i = 0;i< 10 ;i++){
+            arr.add(i);
+        }
+        arr.parallelStream().forEach( index->reader(index,maxMap));
 
         System.out.println("write over");
     }
 
 
-    public static void readerFastP(){
-        for(int i = 0 ;i < 10 ;i++){
-            reader(i);
-        }
-    }
-    public static void reader(Object index ) {
+
+    public static void reader(Object index, HashMap<Integer,Integer> mapMax) {
         int i = (int)index;
         File f = new File("e:/txl/data/data_"+i  +".txt");
 
@@ -64,6 +74,10 @@ public class importData {
                 map.merge(num, 1, (a, b) -> a + b);
             }
 
+            Map.Entry<Integer, Integer> item = map.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).get();
+            Integer flagKey = item.getKey();
+            Integer flagValue = item.getValue();
+            mapMax.put(item.getKey(),item.getValue());
            /* long startime2 = System.currentTimeMillis();
             // refactor
             Map.Entry<Integer, Integer> item = map.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).get();
